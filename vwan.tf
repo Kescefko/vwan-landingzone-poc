d0f99859941a31a33e27cd36001e123a8a7bf6c7
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-15 17:47:20 +0100

    increase timeout at vwan it takes long to deploy

--- vwan.tf
+++ vwan.tf
@@ -2,4 +2,9 @@ resource "azurerm_virtual_wan" "poc_vwan" {
   name                = var.vwan_name
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
+
+  timeouts {
+    create = "60m"   # Increase timeout to 60 minutes
+    delete = "30m"
+  }
 }
