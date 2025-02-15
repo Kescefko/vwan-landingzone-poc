3d2dfdc42f0178f57dcafcc38d8c638583309f9a
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-15 17:22:27 +0100

    add sub id for provider

--- variables.tf
+++ variables.tf
@@ -1,12 +1,20 @@
+variable "subscription_id" {
+  default = "1c2b3b73-e5d4-4ab6-9ac4-c807cb39e0ef"
+  sensitive = true
+}
+
 variable "resource_group_name" {
   default = "rg-gyorgy-shared-dev"
 }
+
 variable "vwan_name" {
   default = "poc-vwan"
 }
+
 variable "vhub_name" {
   default = "poc-vhub"
 }
+
 variable "vnet_name" {
   default = "poc-vnet"
 }
