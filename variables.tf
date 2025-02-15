a5ae118f058262593ae2afd2a966f079d463efa3
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-15 17:17:04 +0100

    add resource group file and variables file

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {
+  default = "poc-rg"
+}
+variable "vwan_name" {
+  default = "poc-vwan"
+}
+variable "vhub_name" {
+  default = "poc-vhub"
+}
+variable "vnet_name" {
+  default = "poc-vnet"
+}
