b25988bb3a51eca1154e490595619f667e5a0032
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-16 20:22:47 +0100

    add firewall policy and rules in hub suinorth

--- firewall_hub_suinorth.tf
+++ firewall_hub_suinorth.tf
@@ -12,3 +12,50 @@ resource "azurerm_firewall" "firewall_hub_suinorth" {
     virtual_hub_id = azurerm_virtual_hub.hub_suinorth.id
   }
 }
+
+# policy
+resource "azurerm_firewall_policy" "firewall_hub_suinorth_policy" {
+  name = "firewall_hub_suinorth_policy"
+  location = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# rules
+resource "azurerm_firewall_policy_rule_collection_group" "firewall_hub_suinorth_policy_rule_group" {
+  name               = "firewall_hub_suinorth_policy_rule_group"
+  firewall_policy_id = azurerm_firewall_policy.firewall_hub_suinorth_policy.id
+  priority           = 100
+
+  application_rule_collection {
+    name     = "AppRules"
+    priority = 100
+    action   = "Allow"
+
+    rule {
+      name = "AllowHTTPSExampleSite"
+      source_addresses = ["10.0.0.0/16"]
+      destination_fqdns = ["www.example.com"]
+      protocols {
+        type = "Https"
+        port = 443
+      }
+    }
+
+    rule {
+      name = "DenyHttp"
+      source_addresses = ["10.0.0.0/16"]
+      destination_fqdns = ["*"]
+      protocols {
+        type = "Http"
+        port = "80"
+      }
+    }
+  }
+}
+
+# Attach Firewall Policy to Firewall
+resource "azurerm_firewall_policy_rule_collection_group" "policy_association" {
+  name               = "firewall-policy-association"
+  firewall_policy_id = azurerm_firewall.firewall_hub_suinorth.id
+  priority           = 200
+}
