e8062853cc67c73baa2514866af05d0ae6f6e432
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-04-23 16:38:32 +0200

    add new rg with vnet snet

--- vnet_suinorth.tf
+++ vnet_suinorth.tf
@@ -1,10 +1,17 @@
-data "azurerm_virtual_network" "vnet_suinorth" {
+resource "azurerm_virtual_network" "vnet_suinorth" {
   name                = var.vnet_name
+  location            = var.location
   resource_group_name = var.resource_group_name
+  address_space       = ["10.101.0.0/24"]  # Safe range, not overlapping with 10.100.0.0/24
+
+  tags = {
+    environment = "poc"
+  }
 }
 
-data "azurerm_subnet" "snet_suinorth_pe" {
-  name = "default"
-  resource_group_name = var.resource_group_name
-  virtual_network_name = data.azurerm_virtual_network.vnet_suinorth.name
+resource "azurerm_subnet" "snet_suinorth_pe" {
+  name                 = "default"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet_suinorth.name
+  address_prefixes     = ["10.101.0.0/27"]
 }
