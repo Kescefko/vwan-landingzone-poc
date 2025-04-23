1e6498b9a81458d56723341ddd648fdeb09f9475
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-04-23 16:45:41 +0200

    fix rg ref

--- firewall_hub_suinorth.tf
+++ firewall_hub_suinorth.tf
@@ -17,7 +17,7 @@ resource "azurerm_firewall" "firewall_hub_suinorth" {
 resource "azurerm_firewall_policy" "firewall_hub_suinorth_policy" {
   name = "firewall_hub_suinorth_policy"
   location = var.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # rules
--- vwan.tf
+++ vwan.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_wan" "poc_vwan" {
   name                = var.vwan_name
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
 
   timeouts {
