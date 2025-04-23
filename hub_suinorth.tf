b25041887541a9d80b9f70e945fa0eaedf12c6f2
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-04-23 16:46:12 +0200

    fix rg ref at hub too

--- hub_suinorth.tf
+++ hub_suinorth.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_hub" "hub_suinorth" {
   depends_on = [ azurerm_virtual_wan.poc_vwan ]
 
   name                = var.vhub_name
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   virtual_wan_id      = azurerm_virtual_wan.poc_vwan.id
   address_prefix      = "10.100.0.0/24"
