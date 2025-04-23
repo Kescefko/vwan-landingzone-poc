3ebf45163ee8c0e4d2fcc05c8d1c2abd8fc604dd
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-04-23 16:41:25 +0200

    fix spoke ref to real vnet id

--- hub_spoke_connection_suinorth.tf
+++ hub_spoke_connection_suinorth.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_hub_connection" "hub_spoke_connection_suinorth" {
   name = "hub_spoke_connection_suinorth"
   virtual_hub_id = azurerm_virtual_hub.hub_suinorth.id
-  remote_virtual_network_id = data.azurerm_virtual_network.vnet_suinorth.id
+  remote_virtual_network_id =azurerm_virtual_network.vnet_suinorth.id
   internet_security_enabled = true
 }
